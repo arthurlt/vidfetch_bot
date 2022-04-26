31d04c00e90b1463288c3fec9e5a92ff1b909173
Arthur Tucker <arthur@tuckerfami.ly>
2022-04-26 17:49:36 -0600

    added reddit functionality.. feels hacky though

--- main.dart
+++ main.dart
@@ -37,11 +37,19 @@ Future<void> main() async {
       disable_notification: true, 
       withQuote: true, 
       caption: await getTiktokTitle(message.text!)));
+
+  teledart
+    .onUrl(RegExp('redd.?it'))
+    .listen((message) async => await getRedditType(message.text!) ?? 
+      message.replyVideo(await redditVideo(message.text!),
+      disable_notification: true,
+      withQuote: true,
+      caption: await getRedditTitle(message.text!)));
 }
 
-Future<dynamic> instagramVideo(String url) async {
+Future<io.File> instagramVideo(String url) async {
   print("received instagram request");
-  final String file = 'instagram_video.mp4';
+  final String file = '/tmp/video.mp4';
   final result = await io.Process.run(
     'yt-dlp', [url,'--force-overwrites', '-o', file]);
   print(result.stdout);
@@ -59,25 +67,55 @@ Future<String> getInstagramTitle(String url, String bibliogramUrl) async {
   return (json['data']['edge_media_to_caption']['edges'][0]['node']['text'].split('\n')[0]);
 }
 
-Future<dynamic> tiktokVideo(String url) async {
+Future<io.File> tiktokVideo(String url) async {
   print("received tiktok request");
-  final String file = 'tiktok_video.mp4';
+  final String file = '/tmp/video.mp4';
   final result = await io.Process.run(
     'yt-dlp', [url, '--force-overwrites', '-o', file]);
   print(result.stdout);
   return(io.File(file));
 }
 
-Future<String> getFullTiktokUrl(String url) async {
+Future<String> getFullURL(String url) async {
   final curlProcess = await io.Process.run(
     'curl', ['-sL', '-w %{url_effective}', '-o /dev/null', url]);
   return(curlProcess.stdout);
 }
 
 Future<String> getTiktokTitle(String url) async {
-  final fullUrl = await getFullTiktokUrl(url);
+  final fullUrl = await getFullURL(url);
   var response = await http.get(
     Uri.parse('https://www.tiktok.com/oembed?url=${fullUrl.replaceAll(' ', '')}'));
   var json = jsonDecode(response.body);
   return(json['title']);
+}
+
+Future<io.File> redditVideo(String url) async {
+  print("received reddit request");
+  final String file = '/tmp/video.mp4';
+  final result = await io.Process.run(
+    'yt-dlp', [url,'--force-overwrites', '-o', file]);
+  print(result.stdout);
+  return(io.File(file));
+}
+
+Future<String> getRedditTitle(String url) async {
+  final fullUrl = await getFullURL(url);
+  var response = await http.get(
+    Uri.parse('${fullUrl.replaceAll(' ', '')}.json'));
+  var json = jsonDecode(response.body);
+  return(json[0]['data']['children'][0]['data']['title']);
+}
+
+Future<bool?> getRedditType(String url) async {
+  final fullUrl = await getFullURL(url);
+  var response = await http.get(
+    Uri.parse('${fullUrl.replaceAll(' ', '')}.json'));
+  var json = jsonDecode(response.body);
+  print('Post type is: ${json[0]['data']['children'][0]['data']['post_hint']}');
+  if (RegExp('video').hasMatch(json[0]['data']['children'][0]['data']['post_hint'])) {
+    return null;
+  } else {
+    return false;
+  }
 }
